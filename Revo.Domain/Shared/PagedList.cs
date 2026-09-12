12b9bc3e23515d4eaf2f610c29fc35ca53b0e61e
Sobih <sobihmohamedsobih@gmail.com>
2026-09-12 06:33:05 +0300

    Add PagedList and ValidationError to Domain.Shared
    Added PagedList<T> for paginated data handling and ValidationError record inheriting from Error, both under the Revo.Domain.Shared namespace. PagedList<T> includes pagination properties and helpers; ValidationError represents validation errors with an array of Error objects.

---
+++ Revo.Domain/Shared/PagedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revo.Domain.Shared
+{
+    public class PagedList<T> where T : class
+    {
+        public List<T> Items {  get; set; } 
+        public int PageIndex { get; set; }
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public bool HasNextPage => PageIndex * PageSize < TotalCount;
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public PagedList(List<T> items, int totalCount, int pageIndex, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+        }
+    }
+}
