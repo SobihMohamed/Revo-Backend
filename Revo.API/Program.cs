0adbba2712b42cd45147934f9a2630186201acd1
Sobih <sobihmohamedsobih@gmail.com>
2026-09-13 21:22:03 +0300

    Refactor API controllers and global exception handling
    - Added BaseApiControllercs for unified API response logic
    - Registered controllers and improved middleware setup in Program.cs
    - Implemented global exception handler and problem details services
    - Limited OpenAPI/Scalar docs to development environments only

--- Revo.API/Program.cs
+++ Revo.API/Program.cs
@@ -4,19 +4,20 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Services Registration
+builder.Services.AddControllers(); 
 builder.Services.AddInfrastructureDependencies(builder.Configuration);
-
 builder.Services.AddOpenApi();
-// Add the global exception handler and problem details middleware
+// Add the global exception handler and problem details services
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
+// 2. HTTP Request Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
     app.MapScalarApiReference(options =>
     {
         options.WithTitle("Revo API Documentation");
@@ -24,9 +25,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseExceptionHandler();
-app.UseRouting();
 
+app.UseExceptionHandler(); 
+
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers(); 
 app.Run();
