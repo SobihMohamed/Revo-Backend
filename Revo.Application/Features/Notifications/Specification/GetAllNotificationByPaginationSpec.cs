bcbedc46d2d713af6353fd4a2c47d4b0e190fec5
Sobih <sobihmohamedsobih@gmail.com>
2026-09-24 18:48:49 +0300

    Add notification DTOs, queries, specs, and tests setup
    Added NotificationDto and NotificationSpecParams for data transfer and filtering. Implemented queries and handlers for fetching and counting notifications, including unread counts. Introduced related specifications for pagination and filtering. Updated test project to include notification feature tests.

---
+++ Revo.Application/Features/Notifications/Specification/GetAllNotificationByPaginationSpec.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.Specification;
+using Revo.Application.Features.Notifications.Dtos;
+using Revo.Application.Features.Notifications.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revo.Application.Features.Notifications.Specification
+{
+    public class GetAllNotificationByPaginationSpec : Specification<Domain.Entities.Notification, NotificationDto>
+    {
+        public GetAllNotificationByPaginationSpec(NotificationSpecParams specParams)
+        {
+            Query
+                .Where(x => !specParams.IsRead.HasValue || x.IsRead == specParams.IsRead.Value)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip((specParams.PageIndex - 1) * specParams.PageSize)
+                .Take(specParams.PageSize);
+
+            Query.Select(n => new NotificationDto
+            {
+                Id = n.Id,
+                TitleAr = n.TitleAr,
+                TitleEn = n.TitleEn,
+                MessageAr = n.MessageAr,
+                MessageEn = n.MessageEn,
+                TargetUrl = n.TargetUrl,
+                IsRead = n.IsRead,
+                CreatedAt = n.CreatedAt
+            });
+        }
+    }
+}
---
+++ Revo.Application/Features/Notifications/Specification/GetNotificationsCountSpec.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Specification;
+using Revo.Application.Features.Notifications.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Revo.Application.Abstraction.Messaging;
+
+namespace Revo.Application.Features.Notifications.Specification
+{
+    public class GetNotificationsCountSpec : Specification<Domain.Entities.Notification>
+    {
+        public GetNotificationsCountSpec(NotificationSpecParams specParams)
+        {
+            Query.Where(x => !specParams.IsRead.HasValue || x.IsRead == specParams.IsRead.Value);
+        }
+    }
+}
